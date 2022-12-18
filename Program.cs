6c6c7c21a3cdc62505e12155c0d3c8200366a9ee
pratik.kanani-at-769440815425 <pratik.kanani@aurionpro.com>
2022-12-18 15:23:53 +0530

    PortChangeRailway

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
 
+var portVar = Environment.GetEnvironmentVariable("PORT");
+
+
+
 WebApplicationBuilder webApplicationBuilder = WebApplication.CreateBuilder(args);
+if(portVar is {Length: >0} && int.TryParse(portVar,out int port))
+{
+    webApplicationBuilder.WebHost.ConfigureKestrel(options =>
+    {
+        options.ListenAnyIP(port);
+    });
+}
 WebApplication webApplication = webApplicationBuilder.Build();
+
 
 webApplication.MapGet("/api/home",(Func<string>)(()=> "Hey listen, Get lost... I Mean Hello World!"));
 webApplication.Run();
