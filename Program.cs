28682acb0fdf623db04cc53943ee4d0aa6bcaba1
pratik.kanani-at-769440815425 <pratik.kanani@aurionpro.com>
2022-12-18 15:00:00 +0530

    Api

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+WebApplicationBuilder webApplicationBuilder = WebApplication.CreateBuilder(args);
+WebApplication webApplication = webApplicationBuilder.Build();
+
+webApplication.MapGet("/api/home",(Func<string>)(()=> "Hey listen, Get lost... I Mean Hello World!"));
+webApplication.Run();
